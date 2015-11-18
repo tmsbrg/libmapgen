13b51a2605b3db42e45364d90e87c003e08fefd3
Thomas van der Berg <ik@thomasvanderberg.nl>
2015-11-18 11:25:29 +0100

    remove things which are Unity-specific (new design)

--- libmapgen/DummyPass.cs
+++ libmapgen/DummyPass.cs
@@ -1,16 +1,8 @@
-using System.Collections.Generic;
 namespace libmapgen
 {
 	public class DummyPass : IMapPass
 	{
 		public DummyPass () { }
-
-		public Dictionary<string, System.Type> getSettings() {
-			return null;
-		}
-
-		public void setSetting(string setting, object value) {
-		}
 
 		public void call(MapArea map) {
 			// do nothing!
--- libmapgen/IMapPass.cs
+++ libmapgen/IMapPass.cs
@@ -1,10 +1,7 @@
-using System.Collections.Generic;
 namespace libmapgen
 {
 	public interface IMapPass
 	{
-		Dictionary<string, System.Type> getSettings();
-		void setSetting(string setting, object value);
 		void call(MapArea map);
 	}
 }
